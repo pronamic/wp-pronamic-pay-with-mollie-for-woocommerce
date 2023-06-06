97a7fb0f17d49101a5d1b84825cbd3b5d65d1a51
Remco Tolsma <869674+remcotolsma@users.noreply.github.com>
2023-06-06 12:30:08 +0200

    Init.

---
+++ pronamic-pay-with-mollie-for-woocommerce.php
@@ -0,0 +1,85 @@
+<?php
+/**
+ * Plugin Name: Pronamic Pay with Mollie for WooCommerce
+ * Plugin URI: https://www.pronamic.eu/plugins/pronamic-pay-with-mollie-for-woocommerce/
+ * Description: Pronamic Pay plugin with Mollie for WooCommerce integration.
+ *
+ * Version: 1.0.0
+ * Requires at least: 5.9
+ * Requires PHP: 7.4
+ *
+ * Author: Pronamic
+ * Author URI: https://www.pronamic.eu/
+ *
+ * Text Domain: pronamic-pay-with-mollie-for-woocommerce
+ * Domain Path: /languages/
+ *
+ * Provides: wp-pay/core
+ *
+ * License: GPL-2.0-or-later
+ *
+ * GitHub URI: https://github.com/pronamic/wp-pronamic-pay-with-mollie-for-woocommerce
+ *
+ * @author    Pronamic <info@pronamic.eu>
+ * @copyright 2005-2023 Pronamic
+ * @license   GPL-3.0-or-later
+ * @package   Pronamic\WordPress\Pay
+ */
+
+/**
+ * Autoload.
+ */
+require_once __DIR__ . '/vendor/autoload_packages.php';
+
+/**
+ * Bootstrap.
+ */
+add_action(
+	'plugins_loaded',
+	function() {
+		load_plugin_textdomain( 'pronamic-pay-with-mollie-for-woocommerce', false, dirname( plugin_basename( __FILE__ ) ) . '/languages' ); 
+	}
+);
+
+\Pronamic\WordPress\Pay\Plugin::instance(
+	[
+		'file'             => __FILE__,
+		'action_scheduler' => __DIR__ . '/packages/woocommerce/action-scheduler/action-scheduler.php',
+	]
+);
+
+add_filter(
+	'pronamic_pay_modules',
+	function( $modules ) {
+		$modules[] = 'subscriptions';
+
+		return $modules;
+	}
+);
+
+add_filter(
+	'pronamic_pay_plugin_integrations',
+	function( $integrations ) {
+		$class = \Pronamic\WordPress\Pay\Extensions\WooCommerce\Extension::class;
+
+		if ( ! array_key_exists( $class, $integrations ) ) {
+			$integrations[ $class ] = new $class();
+		}
+
+		return $integrations;
+	}
+);
+
+add_filter(
+	'pronamic_pay_gateways',
+	function( $gateways ) {
+		$gateways[] = new \Pronamic\WordPress\Pay\Gateways\Mollie\Integration(
+			[
+				'register_url' => 'https://www.mollie.com/nl/signup/665327',
+				'manual_url'   => \__( 'https://www.pronamicpay.com/en/manuals/how-to-connect-mollie-to-wordpress-with-pronamic-pay/', 'pronamic-pay-with-mollie-for-woocommerce' ),
+			]
+		);
+
+		return $gateways;
+	}
+);
---
+++ readme.txt
@@ -0,0 +1,46 @@
+=== Pronamic Pay with Mollie for WooCommerce ===
+Contributors: pronamic
+Tags: mollie, woocommerce, pronamic
+Requires at least: 5.9
+Tested up to: 6.2
+Requires PHP: 7.4
+Stable tag: 1.0.0
+License: GPLv2 or later
+License URI: https://www.gnu.org/licenses/gpl-2.0.html
+
+Connect Mollie to WooCommerce with Pronamic Pay. This free plugin is all that you need to start selling with WooCommerce.
+
+== Description ==
+
+
+
+= Features =
+
+This plugin is developed from the idea that online payments should be easy. Some of the features:
+
+- Easy install
+- Support for 10+ payment methods
+- Payment status pages
+- Automatic payment status updates via Mollie webhooks and the WordPress REST API
+- High quality support
+- Built by Pronamic
+
+== Installation ==
+
+1. To install this plugin you can follow the WordPress documentation section "[Installing Plugins](https://wordpress.org/documentation/article/manage-plugins/#installing-plugins-1)".
+
+== Screenshots ==
+
+1. Payment Gateway Configurations
+2. Edit Payment Gateway Configuration
+
+== Changelog ==
+
+= 1.0.0 =
+
+- First release.
+
+== Links ==
+
+-	[Pronamic Pay](https://www.pronamicpay.com/)
+-	[Pronamic](https://www.pronamic.eu/)
