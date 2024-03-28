dcf85a5a3750b9528f3b3985df657e3833702091
Remco Tolsma <869674+remcotolsma@users.noreply.github.com>
2024-03-28 12:50:10 +0100

    Simplify stable tag update.

--- .github/scripts/deploy-readme-to-wordpress-org.php
+++ .github/scripts/deploy-readme-to-wordpress-org.php
@@ -5,10 +5,7 @@
  */
 $svn_username = getenv( 'SVN_USERNAME' );
 $svn_password = getenv( 'SVN_PASSWORD' );
-$svn_path     = getenv( 'SVN_PATH' );
 $wp_slug      = getenv( 'WP_SLUG' );
-
-$svn_path = empty( $svn_path ) ? 'trunk' : $svn_path;
 
 $svn_url = "https://plugins.svn.wordpress.org/$wp_slug";
 
@@ -21,6 +18,19 @@ unlink( $svn_checkout_dir );
 mkdir( $svn_checkout_dir );
 
 /**
+ * Parse stable tag.
+ */
+$readme_content = file_get_contents( $readme_file );
+
+$pattern = "/Stable tag: (.*)/";
+
+$stable_tag = '';
+
+if ( 1 === preg_match( $pattern, $readme_content, $matches ) ) {
+	$stable_tag = $matches[1];
+}
+
+/**
  * Start.
  */
 echo '🚀 Deploy readme.txt to WordPress.org', PHP_EOL;
@@ -30,19 +40,29 @@ echo '- Subversion username: ', $svn_username, PHP_EOL;
 echo '- Subversion password: ', $svn_password, PHP_EOL;
 echo '- Subversion checkout directory: ', $svn_checkout_dir, PHP_EOL;
 echo '- Path readme.txt: ', $readme_file, PHP_EOL;
+echo '- Stable tag: ', $stable_tag, PHP_EOL;
 
 /**
  * Subversion.
  * 
  * @link https://stackoverflow.com/a/122291
  */
-passthru( "svn checkout $svn_url/$svn_path/ $svn_checkout_dir --depth empty" );
+passthru( "svn checkout $svn_url $svn_checkout_dir --depth empty" );
 
 chdir( $svn_checkout_dir );
 
-passthru( 'svn up readme.txt' );
+passthru( 'svn update trunk --depth=empty' );
+passthru( 'svn update trunk/readme.txt' );
 
-copy( $readme_file, 'readme.txt' );
+copy( $readme_file, 'trunk/readme.txt' );
+
+if ( '' !== $stable_tag ) {
+	passthru( "svn update tags --depth=empty" );
+	passthru( "svn update tags/$stable_tag --depth=empty" );
+	passthru( "svn update tags/$stable_tag/readme.txt --depth=empty" );
+
+	copy( $readme_file, "tags/$stable_tag/readme.txt" );
+}
 
 passthru( "svn commit --message 'Update readme.txt' --non-interactive --username '$svn_username' --password '$svn_password'" );
 
