c73fc1d7293f2b8a12facd4fe9fe74871a865454
Remco Tolsma <869674+remcotolsma@users.noreply.github.com>
2024-03-28 09:57:29 +0100

    Update deploy-readme-to-wordpress-org.php
    Update deploy-readme-to-wordpress-org.php
    
    wip
    
    Update deploy-readme-to-wordpress-org.yml
    
    wip
    
    Create deploy-readme-to-wordpress-org.yml

---
+++ .github/scripts/deploy-readme-to-wordpress-org.php
@@ -0,0 +1,50 @@
+<?php
+
+/**
+ * Setup.
+ */
+$svn_username = getenv( 'SVN_USERNAME' );
+$svn_password = getenv( 'SVN_PASSWORD' );
+
+$wp_slug = getenv( 'WP_SLUG' );
+
+$svn_url = "https://plugins.svn.wordpress.org/$wp_slug";
+
+$readme_file = getcwd() . '/readme.txt';
+
+$svn_checkout_dir = tempnam( sys_get_temp_dir(), '' );
+
+unlink( $svn_checkout_dir );
+
+mkdir( $svn_checkout_dir );
+
+/**
+ * Start.
+ */
+echo '🚀 Deploy readme.txt to WordPress.org', PHP_EOL;
+
+echo '- Subversion URL: ', $svn_url, PHP_EOL;
+echo '- Subversion username: ', $svn_username, PHP_EOL;
+echo '- Subversion password: ', $svn_password, PHP_EOL;
+echo '- Subversion checkout directory: ', $svn_checkout_dir, PHP_EOL;
+echo '- Path readme.txt: ', $readme_file, PHP_EOL;
+
+/**
+ * Subversion.
+ * 
+ * @link https://stackoverflow.com/a/122291
+ */
+passthru( "svn checkout $svn_url/trunk/ $svn_checkout_dir --depth empty" );
+
+chdir( $svn_checkout_dir );
+
+passthru( 'svn up readme.txt' );
+
+copy( $readme_file, 'readme.txt' );
+
+passthru( "svn commit --message 'Update readme.txt' --non-interactive --username '$svn_username' --password '$svn_password'" );
+
+/**
+ * Cleanup.
+ */
+passthru( "rm -f -R $svn_checkout_dir" );
