e0edda3ede51e4814b56153961f7383ad0957ee4
Remco Tolsma <info@remcotolsma.nl>
2025-08-22 14:02:47 +0200

    v1.7.0

--- pronamic-pay-with-mollie-for-woocommerce.php
+++ pronamic-pay-with-mollie-for-woocommerce.php
@@ -4,7 +4,7 @@
  * Plugin URI: https://www.pronamic.eu/plugins/pronamic-pay-with-mollie-for-woocommerce/
  * Description: Pronamic Pay plugin with Mollie for WooCommerce integration.
  *
- * Version: 1.6.0
+ * Version: 1.7.0
  * Requires at least: 5.9
  * Requires PHP: 8.2
  *
